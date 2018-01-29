ad053c7c69edd16cf7437622d457314f55113841
Artyom Rudavin <arrudavin7@gmail.com>
2018-01-29 13:38:23 +0200

    adaptived header, footer, characteristics, recipe, modes, mode 1-8, constructor

--- app/js/common.js
+++ app/js/common.js
@@ -322,6 +322,12 @@ $(function() {
 	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
 	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 75}, 2000); // анимируем скроолинг к элементу scroll_el
+		
+	    if ( $(window).width() < 768 ) {
+		    $('.icon').toggleClass('active');
+			var menu = $('#menu');
+			menu.slideToggle();
+	    };	    
 	}
 	    return false; // выключаем стандартное действие
 	});
